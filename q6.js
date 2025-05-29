7b01aea2dfe2c8e146d9762de3a2709c64ebca1d
rishitha6274 <rishithalattupally01@gmail.com>
2025-05-29 19:52:04 +0530

    updated

---
+++ q6.js
@@ -0,0 +1,24 @@
+db.employees.updateMany(
+  { email: "john@gmail.com" },
+  { $set: { email: "john@outlook.com" } }
+);
+
+
+db.employees.find().sort({ salary: -1 }).limit(3);
+
+
+db.employees.updateMany(
+  {},
+  { $inc: { salary: 1000 } }
+);
+
+db.employees.insertOne({
+    name: "Christy",
+    email: "christy@gmail.com",
+    department: "admin",
+    salary: 2556,
+    location: ["AZ", "TX"],
+    date: Date(),
+})
+
+db.employees.deleteMany({ department: "admin" });
